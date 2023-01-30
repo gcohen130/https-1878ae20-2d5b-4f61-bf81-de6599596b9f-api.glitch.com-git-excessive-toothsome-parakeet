befc75c1851daef748c0918096cd46be3abe2909
Glitch (scrollama-with-bootstrap-boilerplate) <none>
2023-01-30 14:53:12 +0000

    👆✨ Checkpoint ./.glitch-assets:53221619/1930 ./project.js:53221619/546 ./index.html:53221619/740

--- project.js
+++ project.js
@@ -15,7 +15,7 @@ Project.scrolling = {
   // a list of the backdrop images, ordered so they match the `step` elements on the page
   backdrops: [
     {
-      src: "https://cdn.glitch.global/97acdb46-a9d2-4dac-ad23-cb9e010ebc8f/C0007T01.JPG?v=1674670923119",
+      src: "https://cdn.glitch.global/97acdb46-a9d2-4dac-ad23-cb9e010ebc8f/C0026T01.JPG?v=1674671123934",
     },
   ],
 
